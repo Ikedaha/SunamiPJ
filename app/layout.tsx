d7d02331baebd33af2c753f76c2fb6fe59bd0482
Ikedaha <perperper0816@gmail.com>
2025-11-16 03:11:51 +0900

    formatted

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,12 @@
 export const metadata = {
-title: "Wedding Invitation",
-description: "Wedding Party Invitation Page",
+  title: 'Wedding Invitation',
+  description: 'Wedding Party Invitation Page',
 };
 
-
 export default function RootLayout({ children }: { children: React.ReactNode }) {
-return (
-<html lang="ja">
-<body style={{ margin: 0, fontFamily: "sans-serif" }}>{children}</body>
-</html>
-);
+  return (
+    <html lang="ja">
+      <body style={{ margin: 0, fontFamily: 'sans-serif' }}>{children}</body>
+    </html>
+  );
 }
